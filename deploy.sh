5e44c5ff569e5d955bfc9e9abb4ccb822ce7df5c
PhonSopheap <87225124+PhonSopheap@users.noreply.github.com>
2022-09-14 23:01:09 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ sed 's/tageName/$1/g' /home/s0pheap/jenkins/deployment.yaml
 
 #delete image to get the lastest image from dockerhub
 images=`docker images | grep s0pheap/demo-jenkins | awk '{print $3}'`
-docker rmi -f $images
+docker rmi $images
 
 #apply configuration to K8s
 kubectl apply -f /home/s0pheap/jenkins/deployment.yaml
