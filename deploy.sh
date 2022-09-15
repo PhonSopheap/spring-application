7f7a8d724119aaad454949ace7689df4313efa99
PhonSopheap <87225124+PhonSopheap@users.noreply.github.com>
2022-09-15 14:54:05 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sed "s/tageName/$1/g" /home/s0pheap/jenkins/deployment.yaml > /home/s0pheap/jenkins/deployment-spring.yaml 
 
-#apply configuration to K8s
-kubectl apply -f /home/s0pheap/jenkins/deployment-spring.yaml
-
 #delete image to get the lastest image from dockerhub
 images=`docker images | grep s0pheap/demo-jenkins | awk '{print $3}'`
 if [ ! "$images"=="" ]; then
   docker rmi $images
 fi
+
+#apply configuration to K8s
+kubectl apply -f /home/s0pheap/jenkins/deployment-spring.yaml
